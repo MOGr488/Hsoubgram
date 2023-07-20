0af3e10a0ed5c60300a65b8a94b0bc3a18bdebfc
MOGr488 <MOGr488@gmail.com>
2023-07-20 11:12:08 +0400

    03-02 Relationships between Models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Post::factory(10)->create();
+        User::factory()
+            ->count(10)
+            ->has(Post::factory()->count(3))
+            ->create();
+
+
+//        Post::factory(10)->create();
 
 
         // \App\Models\User::factory(10)->create();
