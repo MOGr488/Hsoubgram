768c6c61d5a5d8fbd84c774cac83c37afb6f0b85
mogr488 <mogr488@gmail.com>
2023-09-15 13:00:41 +0400

    05-03.3 Build Auth System (SOLVED)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,7 +10,6 @@
                     </a>
                 </div>
 
-
             </div>
 
             <!-- Settings Dropdown -->
@@ -19,59 +18,55 @@
                     <div class="hidden md:flex md:items-center md:space-x-2">
                         <div class="space-x-3 text-[1.6rem] ltr:mr-5 rtl:ml-5">
                             <a href="/login"
-                               class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest ltr:mr-2 rtl:ml-2">{{ __('Login') }}</a>
+                               class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest ltr:mr-2 rtl:ml-2">{{
+                            __('Login') }}</a>
                             <a href="/register"
-                               class="inline-flex items-center px-4 py-2 dark:text-white font-semibold text-xs uppercase tracking-widest">{{ __('Register') }}</a>
+                               class="inline-flex items-center px-4 py-2 dark:text-white font-semibold text-xs uppercase tracking-widest">{{
+                            __('Register') }}</a>
                         </div>
                     </div>
-                @endguest
-                @auth
+                @else
                     <div class="flex items-center space-x-3">
                         <div class="space-x-3 text-[1.6rem] mr-2 leading-5">
                             <a href="{{ route('home_page') }}">
                                 {!! url()->current() == route('home_page')
-                                ? '<i class="bx bxs-home-alt-2" style="color:#ffffff" ></i>'
+                                ? '<i class="bx bxs-home-alt-2" style="color:#ffffff"></i>'
                                 : '<i class="bx bx-home-alt-2" style="color:#ffffff"></i>' !!}
                             </a>
                             <a href="{{ route('explore') }}">
                                 {!! url()->current() == route('explore')
-                                ? '<i class="bx bxs-compass" style="color:#ffffff" ></i>'
+                                ? '<i class="bx bxs-compass" style="color:#ffffff"></i>'
                                 : '<i class="bx bx-compass" style="color:#ffffff"></i>' !!}
                             </a>
 
                             <a href="{{ route('create_post') }}">
                                 {!! url()->current() == route('create_post')
-                                ? '<i class="bx bxs-message-square-add" style="color:#ffffff" ></i>'
+                                ? '<i class="bx bxs-message-square-add" style="color:#ffffff"></i>'
                                 : '<i class="bx bx-message-square-add" style="color:#ffffff"></i>' !!}
                             </a>
 
 
                         </div>
-                        @endauth
 
                         <div class="hidden md:block">
-                            @auth
                             <x-dropdown align="right" width="48">
                                 <x-slot name="trigger">
                                     <div>
-                                        <img src="{{ auth()->user()->image }}"
-                                             alt="user profile picture"
+                                        <img src="{{ auth()->user()->image }}" alt="user profile picture"
                                              class="w-6 h-6 rounded-full">
                                     </div>
                                 </x-slot>
 
                                 <x-slot name="content">
                                     <x-dropdown-link
-                                        :href="route('user_profile', ['user' => auth()->user()->username])">
+                                            :href="route('user_profile', ['user' => auth()->user()->username])">
                                         {{ __('Profile') }}
                                     </x-dropdown-link>
-                                    @endauth
                                     <!-- Authentication -->
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
 
-                                        <x-dropdown-link :href="route('logout')"
-                                                         onclick="event.preventDefault();
+                                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                             {{ __('Log Out') }}
                                         </x-dropdown-link>
@@ -80,21 +75,21 @@
                             </x-dropdown>
                         </div>
                     </div>
-
-                    <!-- Hamburger -->
-                    <div class="-mr-2 flex items-center sm:hidden">
-                        <button @click="open = ! open"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
-                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
-                                      stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M4 6h16M4 12h16M4 18h16"/>
-                                <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
-                                      stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M6 18L18 6M6 6l12 12"/>
-                            </svg>
-                        </button>
-                    </div>
+                @endguest
+                <!-- Hamburger -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button @click="open = !open"
+                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h16M4 18h16"/>
+                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
+                                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
 
@@ -103,32 +98,31 @@
 
 
             <!-- Responsive Settings Options -->
-
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                 @guest
-                    <x-responsive-nav-link :href="route('login')">{{ __('Login') }}</x-responsive-nav-link>
-                    <x-responsive-nav-link :href="route('register')">{{ __('Register') }}</x-responsive-nav-link>
-                @endguest
+                    <div class="mt-3 space-y-1">
+                        <x-responsive-nav-link :href="route('login')">{{ __('Login') }}</x-responsive-nav-link>
+                        <x-responsive-nav-link :href="route('register')">{{ __('Register') }}</x-responsive-nav-link>
+                    </div>
 
 
-@auth
-                <div class="mt-3 space-y-1">
-                    <x-responsive-nav-link :href="route('user_profile', ['user' => auth()->user()->username])">
-                        {{ __('Profile') }}
-                    </x-responsive-nav-link>
-
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <x-responsive-nav-link :href="route('logout')"
-                                               onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                            {{ __('Log Out') }}
+                @else
+                    <div class="mt-3 space-y-1">
+                        <x-responsive-nav-link :href="route('user_profile', ['user' => auth()->user()->username])">
+                            {{ __('Profile') }}
                         </x-responsive-nav-link>
-                    </form>
-                </div>
-                    @endauth
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-responsive-nav-link>
+                        </form>
+                    </div>
+                @endguest
             </div>
         </div>
     </div>
